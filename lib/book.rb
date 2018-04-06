94a47ef3c6ee839c26f6982441037b6c535bc12b
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-06 02:53:49 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -6,24 +6,7 @@ class Book
   def title
     @title
   end
-  def author=(author)
-    @author = author
-  end
-  def author
-    @author
-  end
-  def page_count=(page_count)
-    @page_count = page_count
-  end
-  def page_count
-    @page_count
-  end
-  def genre=(genre)
-    @genre = genre
-  end
-  def genre
-    @genre
-  end
+  d
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
